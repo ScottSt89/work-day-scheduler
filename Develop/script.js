6988a20bdbdd3364087f00b257db15d5b3bda62d
Scott StJohn <scottst.89@gmail.com>
2022-11-17 15:20:25 -0700

    added js functionality and html elements

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var today = dayjs();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +21,28 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  $("#currentDay").text(today.format("[Today is] MMMM, DD, dddd"));
+
+  //need a for loop that loops from 8-19;
+  var currentHour = today.format("HH");
+  for (var hour = 8; hour < 20; hour++) {
+    //check if current hour is past present or future;
+    var element = $("#hour-" + hour);
+    if (hour < currentHour) {
+      element.addClass("past");
+      element.removeClass("present");
+      element.removeClass("future");
+    } else if (hour == currentHour) {
+      element.removeClass("past");
+      element.addClass("present");
+      element.removeClass("future");
+    } else if (hour > currentHour) {
+      element.removeClass("past");
+      element.removeClass("present");
+      element.addClass("future");
+    }
+    //find element on page that corresponds to that hour;
+
+    //swap class for past, present, or future;
+  }
 });
